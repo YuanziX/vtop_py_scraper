47becc360893368ad71e2ebed48d5eb1ca9ad2bd
AtomicXZ <achubadyal4@gmail.com>
2024-01-24 14:12:29 +0530

    fix login for new vtop page

--- gen_session.py
+++ gen_session.py
@@ -1,79 +1,20 @@
-from typing import Union
-from utils.payloads import get_login_payload
 import aiohttp
 from constants.constants import *
-import json
-import base64
-import numpy as np
-import os
-import io
+from utils.captcha_solver import solve_base64
 import re
-from PIL import Image
 
-
-CAPTCHA_DIM = (180, 45)
-CHARACTER_DIM = (30, 32)
-
-bitmaps_path = os.path.join(os.path.dirname(__file__), 'constants/bitmaps.json')
-BITMAPS = json.load(open(bitmaps_path))
-BITMAPS = {k: np.array(v) for k, v in BITMAPS.items()}
-
-
-def _img_match_percentage(img_char_matrix: np.ndarray, char_matrix: np.ndarray) -> float:
-    match_count = 1
-    mismatch_count = 1
-
-    match_count = np.sum(img_char_matrix == char_matrix)
-    w, h = img_char_matrix.shape
-    mismatch_count = w*h - match_count
-
-    percent_match = match_count / mismatch_count
-    return percent_match
-
-
-def _identify_chars(img: np.ndarray) -> str:
-    img_width = CAPTCHA_DIM[0]
-    char_width = CHARACTER_DIM[0]
-
-    up_thresh, low_thresh = 12, 44
-
-    captcha = ""
-
-    for i in range(0, img_width, char_width):
-        img_char_matrix = img[up_thresh:low_thresh, i: i+char_width]
-        matches = {}
-        global BITMAPS
-        for char, char_matrix in BITMAPS.items():
-            perc = _img_match_percentage(img_char_matrix, char_matrix)
-            matches.update({perc: char.upper()})
-        try:
-            captcha += matches[max(matches.keys())]
-        except ValueError:
-            captcha += "0"
-    return captcha
-
-
-def _solve_captcha(src: str) -> Union[str, None]:
-    im = base64.b64decode(src)
-    img = Image.open(io.BytesIO(im)).convert("L")
-    img = np.array(img)
-    if img is None:
-        print("Captcha not found, returning .... None")
-        return None
-    captcha = None
+async def gen_session(sess: aiohttp.ClientSession, username: str, password: str):
     try:
-        captcha = _identify_chars(img)
-    except Exception as e:
-        print(e)
-    return captcha
+        async with sess.get("https://vtop.vitap.ac.in/vtop/", headers=user_agent_header):
+            async with sess.get("https://vtop.vitap.ac.in/vtop/open/page") as csrf:
+                csrf_token = re.search(r'name="_csrf" value="(.*)"', await csrf.text()).group(1)
+                await sess.post("https://vtop.vitap.ac.in/vtop/prelogin/setup", data={'_csrf': csrf_token, 'flag': 'VTOP'})
+                await sess.get("https://vtop.vitap.ac.in/vtop/init/page")
+                async with sess.get("https://vtop.vitap.ac.in/vtop/login") as req:
+                    captcha = solve_base64(re.search(r';base64,(.+)"', await req.text()).group(1))
+                    print(captcha)
+                    async with sess.post("https://vtop.vitap.ac.in/vtop/login", data={'_csrf': csrf_token, 'username': username, 'password': password, 'captchaStr': captcha}) as final:
+                        return re.search(r'var csrfValue = "(.*)";', await final.text()).group(1)
+    except:
+        await gen_session(sess, username, password)
 
-
-async def gen_session(sess: aiohttp.ClientSession, uname: str, passwd: str) -> bool:
-    await sess.get(vtop_base_url, headers=user_agent_header)
-    async with sess.post(vtop_login_url) as req:
-        captcha = _solve_captcha(re.search(r';base64, (.+)" />', await req.text()).group(1))
-        async with sess.post(vtop_doLogin_url, data=get_login_payload(uname, passwd, captcha)) as resp:
-            text = await resp.text()
-            if ('Invalid' in text or 'exception' in text or 'inactivity' in text):
-                return False
-            return True
---
+++ utils/captcha_solver.py
@@ -0,0 +1,80 @@
+import numpy as np
+from PIL import Image
+import json
+from constants.bitmaps import bitmaps
+import base64
+from io import BytesIO
+
+def pre_img(img):
+    avg = sum(sum(e) for e in img) / (24 * 22)
+    bits = [[1 if val > avg else 0 for val in row] for row in img]
+    return bits
+
+
+def saturation(d):
+    saturate = np.round(((np.max(d, axis=1) - np.min(d, axis=1)) * 255) / np.max(d, axis=1))
+    img = saturate.reshape((40, 200))
+    bls = [img[7 + 5 * (i % 2) + 1:35 - 5 * ((i + 1) % 2), (i + 1) * 25 + 2:(i + 2) * 25 + 1] for i in range(6)]
+    return bls
+
+
+def flatten(arr):
+    return [val for sublist in arr for val in sublist]
+
+
+def mat_mul(a, b):
+    x, z, y = len(a), len(a[0]), len(b[0])
+    product_row = [0] * y
+    product = [[0] * y for _ in range(x)]
+
+    for i in range(x):
+        for j in range(y):
+            for k in range(z):
+                product[i][j] += a[i][k] * b[k][j]
+
+    return product
+
+
+def mat_add(a, b):
+    return [a[i] + b[i] for i in range(len(a))]
+
+
+def max_soft(a):
+    n = list(a)
+    s = sum(np.exp(f) for f in n)
+    n = [np.exp(f) / s for f in n]
+    return n
+
+
+HEIGHT = 40
+WIDTH = 200
+
+def solve(img):
+    weights = None
+    biases = None
+    label_txt = "ABCDEFGHJKLMNPQRSTUVWXYZ23456789"
+    
+    with open("constants/weights.json", "r") as f:
+        data = json.load(f)
+        weights = data["weights"]
+        biases = data["biases"]
+
+    img_data = img.convert("RGB").getdata()
+    img_array = np.array(list(img_data))
+
+    bls = saturation(img_array)
+    out = ""
+
+    for i in range(6):
+        bls[i] = pre_img(bls[i])
+        bls[i] = [flatten(bls[i])]
+        bls[i] = mat_mul(bls[i], weights)
+        bls[i] = mat_add(*bls[i], biases)
+        bls[i] = max_soft(bls[i])
+        index = bls[i].index(max(bls[i]))
+        out += label_txt[index]
+
+    return out
+
+def solve_base64(img_base64: str) -> str:
+    return solve(Image.open(BytesIO(base64.b64decode(img_base64))))
